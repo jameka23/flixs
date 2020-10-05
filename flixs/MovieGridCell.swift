4ea6b84c87beb2cac23fe498fc5ec63195455877
Jameka Echols <jameka.echols@gmail.com>
2020-10-05 01:15:31 -0500

    issues arrise

---
+++ flixs/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixs
+//
+//  Created by Jameka Echols on 10/5/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- flixs/MoviesViewController.swift
+++ flixs/MoviesViewController.swift
@@ -65,18 +65,30 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         
-        cell.posterView.af_setImage(withURL: posterUrl!)
+        cell.posterView.af.setImage(withURL: posterUrl!)
         return cell
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        print("hi, loading the details")
+        
+        //find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        // pass the selected movie to the details controller
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        //deselect the highlighted row after returning to the table view unselected
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-    */
+    
 
 }
